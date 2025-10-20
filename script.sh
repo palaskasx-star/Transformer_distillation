b56680636ad1a346223d1f370cee3107534c519e
palaskasc <palaskasx@gmail.com>
2025-10-20 16:40:48 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,9 +3,4 @@
 
 source /home/jupyter/palaskasc/Transformer_distillation/CHAD/bin/activate
 
-cd ..
-cd manifold-distillation
-
-torchrun --standalone --nproc_per_node=8 --master_port=29500 main.py --dist_url env:// --distributed --output_dir ../Transformer_distillation/cait_s24_224_100_epochs_single_loss/manifold_distillation --data-path /home/jupyter/imagenet --teacher-path /home/jupyter/palaskasc/Transformer_distillation/S24_224.pth --model deit_tiny_patch16_224 --teacher-model cait_s24_224 --distillation-type soft --epochs 100 --distillation-alpha 1 --distillation-beta 1 --w-sample 0.1 --w-patch 4 --w-rand 0.2 --K 192 --s-id 0 1 2 3 8 9 10 11 --t-id 0 1 2 3 20 21 22 23 --drop-path 0 
-
-
+torchrun --standalone --nproc_per_node=8 --master_port=29500 main.py --dist_url env://  --distributed --data-path /home/jupyter/imagenet --teacher-path /home/jupyter/palaskasc/Transformer_distillation/S24_224.pth --batch-size 64 --model deit_tiny_patch16_224 --teacher-model cait_s24_224 --distillation-type soft --distillation-alpha 1 --distillation-beta 0 --gamma 5 --delta 0 --K 192 --s-id 11 --t-id 23 --drop-path 0 --output_dir ./cait_s24_224_100_epochs_single_loss --num_workers 12 --epochs 100 --normalize --distance KL --use-prototypes --prototypes-number 1024
