ee5cd539bd2f5cb8248c45062d96d8a631fb2367
palaskasc <palaskasx@gmail.com>
2025-10-22 23:58:01 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@
 
 source /home/jupyter/palaskasc/Transformer_distillation/CHAD/bin/activate
 
-torchrun --standalone --nproc_per_node=8 --master_port=29500 main.py --dist_url env://  --distributed --data-path /home/jupyter/imagenet --teacher-path /home/jupyter/palaskasc/Transformer_distillation/S24_224.pth --batch-size 64 --model deit_tiny_patch16_224 --teacher-model cait_s24_224 --distillation-type soft --distillation-alpha 1 --distillation-beta 0.22 --gamma 2,34 --delta 0 --K 192 --s-id 11 --t-id 23 --drop-path 0 --output_dir ./cait_s24_224_100_epochs_single_loss --num_workers 12 --epochs 100 --normalize --distance KL --use-prototypes --prototypes-number 1024
 torchrun --standalone --nproc_per_node=8 --master_port=29500 main.py --dist_url env://  --distributed --data-path /home/jupyter/imagenet --teacher-path /home/jupyter/palaskasc/Transformer_distillation/S24_224.pth --batch-size 64 --model deit_tiny_patch16_224 --teacher-model cait_s24_224 --distillation-type soft --distillation-alpha 1 --distillation-beta 0.17 --gamma 1.77 --delta 0.24 --K 192 --s-id 11 --t-id 23 --drop-path 0 --output_dir ./cait_s24_224_100_epochs_single_loss --num_workers 12 --epochs 100 --normalize --distance KL --use-prototypes --prototypes-number 1024
+
