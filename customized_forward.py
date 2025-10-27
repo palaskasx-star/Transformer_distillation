6bb6a07112d4a6fe015c7f1e54c2f64a9328f54e
palaskasc <palaskasx@gmail.com>
2025-10-28 01:12:05 +0200

    Update customized_forward.py

--- customized_forward.py
+++ customized_forward.py
@@ -68,19 +68,19 @@ def vit_forward(self, x, require_feat: bool = True):
         x = outs[0]
         block_outs = outs[-1]
         if self.head_dist is not None:
-            x, x_dist = self.head(x[0].detach()), self.head_dist(x[1].detach())  # x must be a tuple
+            x, x_dist = self.head(x[0]), self.head_dist(x[1])  # x must be a tuple
             if self.training and not torch.jit.is_scripting():
                 # during inference, return the average of both classifier predictions
                 return (x, x_dist), block_outs
             else:
                 return (x + x_dist) / 2, block_outs
         else:
-            x = self.head(x.detach())
+            x = self.head(x)
         return x, block_outs
     else:
         x = self.forward_features(x)
         if self.head_dist is not None:
-            x, x_dist = self.head(x[0].detach()), self.head_dist(x[1].detach())  # x must be a tuple
+            x, x_dist = self.head(x[0]), self.head_dist(x[1])  # x must be a tuple
             if self.training and not torch.jit.is_scripting():
                 # during inference, return the average of both classifier predictions
                 return x, x_dist
@@ -166,5 +166,6 @@ def regnet_forward(self, x, require_feat: bool = True):
         return logits, feats
     else:
         return self.forward_features(x)
+
 
 
