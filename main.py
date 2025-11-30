af16490a674f7b0ce448b336f7aeb407bec342f3
palaskasc <palaskasx@gmail.com>
2025-12-01 00:26:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -541,7 +541,7 @@ def main(args):
                         'args': args,
                     }, checkpoint_path)     
 
-        test_stats = evaluate(data_loader_val, model, device, writer, epoch)
+        test_stats = evaluate(data_loader_val, model, device, criterion, writer, epoch)
         print(f"Accuracy of the network on the {len(dataset_val)} test images: {test_stats['acc1']:.1f}%")
         
         if max_accuracy < test_stats["acc1"]:
@@ -586,6 +586,7 @@ if __name__ == '__main__':
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
 
     main(args)
+
 
 
 
