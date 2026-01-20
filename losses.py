3480684535940a81525a60b8709c2de8905f241c
palaskasc <palaskasx@gmail.com>
2026-01-20 21:17:23 +0200

    Update losses.py

--- losses.py
+++ losses.py
@@ -23,10 +23,17 @@ class DistillationLoss(nn.Module):
     taking a teacher model prediction and using it as additional supervision.
     """
 
-    def __init__(self, base_criterion: torch.nn.Module, teacher_model: torch.nn.Module, prototypes: None, projectors_nets: None, args):
+    def __init__(self, 
+             base_criterion: torch.nn.Module, 
+             teacher_model: torch.nn.Module, 
+             args,
+             proto_proj_module: torch.nn.Module = None,
+             vitkd_projector: torch.nn.Module = None):
         super().__init__()
+
         self.base_criterion = base_criterion
         self.teacher_model = teacher_model
+        print(args.distillation_type)
         assert args.distillation_type in ['none', 'soft', 'hard']
         self.distillation_type = args.distillation_type
         self.tau = args.distillation_tau
@@ -40,13 +47,21 @@ class DistillationLoss(nn.Module):
         self.normalize = args.normalize
         self.distance = args.distance
 
-        self.prototypes = prototypes
-        self.projectors_nets = projectors_nets
+        if proto_proj_module is not None:
+            self.prototypes = proto_proj_module.prototypes
+            self.projectors_nets = proto_proj_module.projectors
 
         self.world_size = args.world_size
 
         self.KoLeoData = KoLeoLossData()
         self.KoLeoPrototypes = KoLeoLossPrototypes()
+
+        self.use_vitkd = args.use_vitkd
+        self.vitkd_projector = vitkd_projector
+        if self.use_vitkd:
+            self.alpha_vitkd = getattr(args, 'alpha_vitkd', 0.00003)
+            self.beta_vitkd = getattr(args, 'beta_vitkd', 0.000003)
+            self.lambda_vitkd = getattr(args, 'lambda_vitkd', 0.5)
 
     def forward(self, inputs, outputs, labels):
         """
@@ -87,11 +102,23 @@ class DistillationLoss(nn.Module):
 
         loss_base = base_loss
         loss_dist = distillation_loss
-        loss_mf_patch, loss_mf_cls, loss_mf_rand, loss_KoLeo_patch_data, loss_KoLeo_cls_data, loss_KoLeo_rand_data, loss_KoLeo_patch_proto, loss_KoLeo_cls_proto, loss_KoLeo_rand_proto = mf_loss(block_outs_s, block_outs_t, self.layer_ids_s,
-                                  self.layer_ids_t, self.K, normalize=self.normalize, distance=self.distance,
-                                  prototypes=self.prototypes, projectors_nets=self.projectors_nets, KoLeoData=self.KoLeoData, KoLeoPrototypes=self.KoLeoPrototypes, world_size=self.world_size)  # manifold distillation loss
 
-        return loss_base, loss_dist, loss_mf_patch, loss_mf_cls, loss_mf_rand, loss_KoLeo_patch_data, loss_KoLeo_cls_data, loss_KoLeo_rand_data, loss_KoLeo_patch_proto, loss_KoLeo_cls_proto, loss_KoLeo_rand_proto
+        if self.use_vitkd:
+            loss_vitkd = vitkd_solver(
+                block_outs_s, 
+                block_outs_t, 
+                self.vitkd_projector,
+                self.alpha_vitkd,
+                self.beta_vitkd,
+                self.lambda_vitkd
+            )
+            return loss_base, loss_dist, loss_vitkd,  torch.tensor(0.0, device=loss_base.device), torch.tensor(0.0, device=loss_base.device), torch.tensor(0.0, device=loss_base.device), torch.tensor(0.0, device=loss_base.device), torch.tensor(0.0, device=loss_base.device), torch.tensor(0.0, device=loss_base.device), torch.tensor(0.0, device=loss_base.device), torch.tensor(0.0, device=loss_base.device)
+        else:
+            loss_mf_patch, loss_mf_cls, loss_mf_rand, loss_KoLeo_patch_data, loss_KoLeo_cls_data, loss_KoLeo_rand_data, loss_KoLeo_patch_proto, loss_KoLeo_cls_proto, loss_KoLeo_rand_proto = mf_loss(block_outs_s, block_outs_t, self.layer_ids_s,
+                                    self.layer_ids_t, self.K, normalize=self.normalize, distance=self.distance,
+                                    prototypes=self.prototypes, projectors_nets=self.projectors_nets, KoLeoData=self.KoLeoData, KoLeoPrototypes=self.KoLeoPrototypes, world_size=self.world_size)  # manifold distillation loss
+
+            return loss_base, loss_dist, loss_mf_patch, loss_mf_cls, loss_mf_rand, loss_KoLeo_patch_data, loss_KoLeo_cls_data, loss_KoLeo_rand_data, loss_KoLeo_patch_proto, loss_KoLeo_cls_proto, loss_KoLeo_rand_proto
 
 
 def mf_loss(block_outs_s, block_outs_t, layer_ids_s, layer_ids_t, K, max_patch_num=0, normalize=False, distance='MSE', prototypes=None, projectors_nets=None, KoLeoData=None, KoLeoPrototypes=None, world_size=1):
@@ -137,7 +164,6 @@ def mf_loss(block_outs_s, block_outs_t, layer_ids_s, layer_ids_t, K, max_patch_n
     loss_KoLeo_rand_proto = sum(losses_KoLeo_proto[2]) / len(losses_KoLeo_proto[2])
 
     return loss_mf_patch, loss_mf_cls, loss_mf_rand, loss_KoLeo_patch_data, loss_KoLeo_cls_data, loss_KoLeo_rand_data, loss_KoLeo_patch_proto, loss_KoLeo_cls_proto, loss_KoLeo_rand_proto
-
 
 def layer_mf_loss(F_s, F_t, K, normalize=False, distance='MSE', temperature=0.1, eps=1e-8, prototypes=None, projectors_net=None):
     # intra-image manifold loss
@@ -327,8 +353,6 @@ def layer_mf_loss_prototypes(F_s, F_t, K, normalize=False, distance='MSE', eps=1
 
     return loss_mf_patch, loss_mf_cls, loss_mf_rand, loss_KoLeo_patch_data, loss_KoLeo_cls_data, loss_KoLeo_rand_data, loss_KoLeo_patch_proto, loss_KoLeo_cls_proto, loss_KoLeo_rand_proto
 
-
-
 def merge(x, max_patch_num=196):
     B, P, C = x.shape
     if P <= max_patch_num:
@@ -339,7 +363,6 @@ def merge(x, max_patch_num=196):
     x = x.view(B, m, merge_num, m, merge_num, C)
     merged = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, m * m, -1)
     return merged
-
 
 @torch.no_grad()
 def sinkhorn(out, nmb_iters=3, epsilon=0.05):
@@ -389,6 +412,93 @@ def distributed_sinkhorn(out, nmb_iters=3, epsilon=0.05, world_size=1):
 
     Q *= B # the colomns must sum to 1 so that Q is an assignment
     return Q.permute(0, 2, 1)
+
+def vitkd_solver(
+                preds_S, 
+                preds_T, 
+                vitkd_projector,
+                alpha_vitkd,
+                beta_vitkd,
+                lambda_vitkd
+            ):
+    """Forward function.
+    Args:
+        preds_S(List): [B*2*N*D, B*N*D], student's feature map
+        preds_T(List): [B*2*N*D, B*N*D], teacher's feature map
+    """
+
+    low_s = torch.stack([preds_S[0][:,1:,:], preds_S[1][:,1:,:]], dim=1)
+    low_t = torch.stack([preds_T[0][:,1:,:], preds_T[1][:,1:,:]], dim=1)
+
+    high_s = preds_S[-1][:,1:,:]
+    high_t = preds_T[-1][:,1:,:]
+
+    B = low_s.shape[0]
+    loss_mse = nn.MSELoss(reduction='sum')
+
+    '''ViTKD: Mimicking'''
+    if vitkd_projector.align2 is not None:
+        for i in range(2):
+            if i == 0:
+                xc = vitkd_projector.align2[i](low_s[:,i]).unsqueeze(1)
+            else:
+                xc = torch.cat((xc, vitkd_projector.align2[i](low_s[:,i]).unsqueeze(1)),dim=1)
+    else:
+        xc = low_s
+
+    loss_lr = loss_mse(xc, low_t) / B * alpha_vitkd
+
+    '''ViTKD: Generation'''
+    if vitkd_projector.align is not None:
+        x = vitkd_projector.align(high_s)
+    else:
+        x = high_s
+
+    # Mask tokens
+    B, N, D = x.shape
+    x, mat, ids, ids_masked = random_masking(x,lambda_vitkd)
+    mask_tokens = vitkd_projector.mask_token.repeat(B, N - x.shape[1], 1)
+    x_ = torch.cat([x, mask_tokens], dim=1)
+    x = torch.gather(x_, dim=1, index=ids.unsqueeze(-1).repeat(1, 1, D))
+    mask = mat.unsqueeze(-1)
+
+    hw = int(N**0.5)
+    x = x.reshape(B, hw, hw, D).permute(0, 3, 1, 2)
+    x = vitkd_projector.generation(x).flatten(2).transpose(1,2)
+
+    loss_gen = loss_mse(torch.mul(x, mask), torch.mul(high_t, mask))
+    loss_gen = loss_gen / B * beta_vitkd / lambda_vitkd
+        
+    return loss_lr + loss_gen
+
+def random_masking(x, mask_ratio):
+    """
+    Perform per-sample random masking by per-sample shuffling.
+    Per-sample shuffling is done by argsort random noise.
+    x: [N, L, D], sequence
+    """
+    N, L, D = x.shape  # batch, length, dim
+    len_keep = int(L * (1 - mask_ratio))
+    
+    noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
+    
+    # sort noise for each sample
+    ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
+    ids_restore = torch.argsort(ids_shuffle, dim=1)
+
+    # keep the first subset
+    ids_keep = ids_shuffle[:, :len_keep]
+    ids_masked = ids_shuffle[:, len_keep:L]
+
+    x_keep = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
+
+    # generate the binary mask: 0 is keep, 1 is remove
+    mask = torch.ones([N, L], device=x.device)
+    mask[:, :len_keep] = 0
+    # unshuffle to get the binary mask
+    mask = torch.gather(mask, dim=1, index=ids_restore)
+
+    return x_keep, mask, ids_restore, ids_masked
 
 
 class KoLeoLossData(nn.Module):
