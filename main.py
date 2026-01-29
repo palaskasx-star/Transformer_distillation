5a7f58b0df22d14befb019315aedde81b96d735b
palaskasc <palaskasx@gmail.com>
2026-01-29 19:23:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -284,6 +284,8 @@ def main(args):
     extra_info = f"model_{args.model}_teacher_{args.teacher_model}_bs_{num_tasks*args.batch_size}_proj_{args.projector_type}_normalize_{args.normalize}_distance_{args.distance}_distype_{args.distillation_type}_cj_{args.color_jitter}_alpha_{args.distillation_alpha}_beta_{args.distillation_beta}_gamma_{args.gamma}_delta_{args.delta}_KoLeoD_{args.KoLeoData}_KoLeoP_{args.KoLeoPrototypes}_K_{args.K}_sids_{''.join(map(str, args.s_id))}_tids_{''.join(map(str, args.t_id))}"
     if args.use_prototypes:
         extra_info += f"_prototypes_{args.prototypes_number}"
+    if args.PLD:
+        extra_info += f"_PLD"
     output_dir = output_dir / extra_info
     output_dir.mkdir(parents=True, exist_ok=True)
 
@@ -655,3 +657,4 @@ if __name__ == '__main__':
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
 
     main(args)
+
