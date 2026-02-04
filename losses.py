a1dbdf9ace89235f9f97a1252e4a0bdb5eacc87d
palaskasc <palaskasx@gmail.com>
2026-02-04 15:26:20 +0200

    Update losses.py

--- losses.py
+++ losses.py
@@ -280,7 +280,7 @@ def layer_mf_loss_prototypes_rand(F_s, F_t, K, normalize=False, distance='MSE', 
 
     # cls token loss
     F_s = F_s[:, 1:, :].clone()  # select only the cls token
-    F_t = F_t[:, 2:, :].clone()  # select only the cls token
+    F_t = F_t[:, 1:, :].clone()  # select only the cls token
     
     # manifold loss among random sampled patches
     bsz, patch_num, _ = F_s.shape
