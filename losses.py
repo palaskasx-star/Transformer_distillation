591aeae511091f0c8cef2dd2527b8c12cc802bc9
palaskasc <palaskasx@gmail.com>
2026-02-07 23:00:44 +0200

    Update losses.py

--- losses.py
+++ losses.py
@@ -296,18 +296,19 @@ def layer_mf_loss_prototypes_rand(F_s, F_t, K, normalize=False, distance='MSE', 
     #loss_KoLeo_rand_data = KoLeoData(f_s)
     #loss_KoLeo_rand_proto = KoLeoPrototypes( prototypes.protos[2])
 
-    M_s = gaussian_kernel(f_s, protos_norm) + cosine_kernel(f_s, protos_norm)
-    q1 = distributed_sinkhorn(M_s, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
-    M_t = gaussian_kernel(f_t, protos_norm) + cosine_kernel(f_t, protos_norm)
-    q2 = distributed_sinkhorn(M_t, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
+    M_s = gaussian_kernel(f_s, protos_norm)
+    #q1 = distributed_sinkhorn(M_s, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
+    M_t = gaussian_kernel(f_t, protos_norm)
+    #q2 = distributed_sinkhorn(M_t, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
 
     p1 = F.softmax(M_s / temperature, dim=2)
     p2 = F.softmax(M_t / temperature, dim=2)
 
-    loss12 = - torch.mean(torch.sum(q1 * torch.log(p2 + 1e-6), dim=2))
-    loss21 = - torch.mean(torch.sum(q2 * torch.log(p1 + 1e-6), dim=2))
+    #loss12 = - torch.mean(torch.sum(q1 * torch.log(p2 + 1e-6), dim=2))
+    #loss21 = - torch.mean(torch.sum(q2 * torch.log(p1 + 1e-6), dim=2))
 
-    loss_mf_rand = (loss12 + loss21)/2
+    #loss_mf_rand = (loss12 + loss21)/2
+    loss_mf_rand = - torch.mean(torch.sum(p2 * torch.log(p1 + 1e-6), dim=2))
 
     dev = loss_mf_rand.device
 
