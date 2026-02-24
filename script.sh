578972de1b1570543134d98031fdfb3793cf0ccd
palaskasc <palaskasx@gmail.com>
2026-02-24 22:15:47 +0200

    Delete script.sh

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Activate the virtual environment
-
-source /home/jupyter/palaskasc/Transformer_distillation/CHAD/bin/activate
-
-torchrun --standalone --nproc_per_node=4 --master_port=29500 main.py --dist_url env://  --distributed --data-path /home/jupyter/imagenet --teacher-path /home/jupyter/palaskasc/Transformer_distillation/S24_224.pth --batch-size 128 --model deit_tiny_patch16_224 --teacher-model cait_s24_224 --distillation-type soft --distillation-alpha 1 --distillation-beta 0 --gamma 3 --delta 0 --K 192 --s-id 11 --t-id 23 --drop-path 0 --output_dir ./report --num_workers 12 --epochs 300 --normalize --distance KL --use-prototypes --prototypes-number 1024
-
