4e603dd3280e204f6da1d699d57c7d455af3a8cf
palaskasc <palaskasx@gmail.com>
2026-03-29 01:57:01 +0200

    Update losses.py

--- losses.py
+++ losses.py
@@ -298,12 +298,12 @@ def layer_mf_loss_prototypes_rand(F_s, F_t, K, normalize=False, distance='MSE', 
     #loss_KoLeo_rand_data = KoLeoData(f_s)
     #loss_KoLeo_rand_proto = KoLeoPrototypes( prototypes.protos[2])
 
-    M_s = L2_dist(f_s, protos_norm)
-    #M_s = -cosine_kernel(f_s, protos_norm)
-    q1 = distributed_sinkhorn(M_s, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
-    M_t = L2_dist(f_t, protos_norm)
-    #M_t = -cosine_kernel(f_t, protos_norm)
-    q2 = distributed_sinkhorn(M_t, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
+    #M_s = L2_dist(f_s, protos_norm)
+    M_s = -cosine_kernel(f_s, f_s)
+    #q1 = distributed_sinkhorn(M_s, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
+    #M_t = L2_dist(f_t, protos_norm)
+    M_t = -cosine_kernel(f_t, f_t)
+    #q2 = distributed_sinkhorn(M_t, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
 
     # cgange this from 1 to temperature
     p1 = F.softmax(-M_s / 1, dim=2)
@@ -321,7 +321,7 @@ def layer_mf_loss_prototypes_rand(F_s, F_t, K, normalize=False, distance='MSE', 
         loss21 = - torch.mean(torch.sum(p2 * torch.log(p1 + 1e-6), dim=2))
         
 
-    loss_mf_rand = (loss21 + 0)/2
+    loss_mf_rand = (0 + loss21)/2
     dev = loss_mf_rand.device
 
     return loss_mf_rand, torch.tensor(0.0, device=dev), torch.tensor(0.0, device=dev)
