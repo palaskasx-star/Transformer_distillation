d6ecabec381441b77ceefb01b65958c0b3ad1924
palaskasc <palaskasx@gmail.com>
2026-04-02 21:18:24 +0300

    Update losses.py

--- losses.py
+++ losses.py
@@ -293,6 +293,7 @@ def layer_mf_loss_prototypes_rand(F_s, F_t, K, normalize=False, distance='MSE', 
 
     M_s_detached_prot = L2_dist(f_s, protos_norm.detach())
     M_s_detached_patches = L2_dist(f_s.detach(), protos_norm)
+    M_s = L2_dist(f_s.detach(), protos_norm)
     #M_s = -cosine_kernel(f_s, protos_norm)
     q1 = distributed_sinkhorn(M_s, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
     M_t_detached_prot = L2_dist(f_t, protos_norm.detach())
