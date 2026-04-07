5642762e2b9a7cb58138edc74f4304c61f15d6c7
palaskasc <palaskasx@gmail.com>
2026-04-08 02:10:39 +0300

    Update losses.py

--- losses.py
+++ losses.py
@@ -300,14 +300,14 @@ def layer_mf_loss_prototypes_rand(F_s, F_t, K, normalize=False, distance='MSE', 
     #loss_KoLeo_rand_data = KoLeoData(f_s)
     #loss_KoLeo_rand_proto = KoLeoPrototypes( prototypes.protos[2])
 
-    #M_s = L2_dist(f_s, protos_norm)
+    M_s = L2_dist(f_s, protos_norm)
     #M_s = -cosine_kernel(f_s, protos_norm)
-    #q1 = distributed_sinkhorn(M_s, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
+    q1 = distributed_sinkhorn(M_s, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
     M_t = L2_dist(f_t, protos_norm)
     #M_t = -cosine_kernel(f_t, protos_norm)
     q2 = distributed_sinkhorn(M_t, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
 
-    #p1 = F.softmax(-M_s / temperature, dim=2)
+    p1 = F.softmax(-M_s / temperature, dim=2)
     p2 = F.softmax(-M_t / temperature, dim=2)
 
     if distance == 'MSE':
@@ -316,10 +316,8 @@ def layer_mf_loss_prototypes_rand(F_s, F_t, K, normalize=False, distance='MSE', 
         loss12 = (diff12 * diff12).mean()
         loss21 = (diff21 * diff21).mean()
     elif distance == 'KL':
-        #loss12 = - torch.mean(torch.sum(q1 * torch.log(p2 + 1e-6), dim=2))
-        #loss21 = - torch.mean(torch.sum(q2 * torch.log(p1 + 1e-6), dim=2))
-        loss12 = torch.mean(torch.sum(q2 * torch.log(M_t), dim=2))
-        loss21 = 0
+        loss1 = - torch.mean(torch.sum(q1 * torch.log(p2 + 1e-6), dim=2))
+        loss2 = - torch.mean(torch.sum(q2 * torch.log(p1 + 1e-6), dim=2))
         
     loss_mf_rand = (2*loss12 + loss21)/2 
     dev = loss_mf_rand.device
