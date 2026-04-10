a353a6f69b9ce12fdd6c6555a5fd0454a561cbf8
palaskasc <palaskasx@gmail.com>
2026-04-11 02:05:56 +0300

    Update losses.py

--- losses.py
+++ losses.py
@@ -274,7 +274,7 @@ def layer_mf_loss_rand(F_s, F_t, K, normalize=False, distance='MSE', temperature
     return loss_mf_rand, torch.tensor(0.0, device=dev), torch.tensor(0.0, device=dev)
 
 
-def layer_mf_loss_prototypes_rand(F_s, F_t, K, normalize=False, distance='MSE', eps=1e-8, prototypes=None, projectors_net=None, KoLeoData=None, KoLeoPrototypes=None, temperature=1, world_size=1):
+def layer_mf_loss_prototypes_rand(F_s, F_t, K, normalize=False, distance='MSE', eps=1e-8, prototypes=None, projectors_net=None, KoLeoData=None, KoLeoPrototypes=None, temperature=0.1, world_size=1):
     bsz, patch_num, _ = F_s.shape
     sampler = torch.randperm(bsz * patch_num)[:K]
     
@@ -300,24 +300,24 @@ def layer_mf_loss_prototypes_rand(F_s, F_t, K, normalize=False, distance='MSE', 
     #M_t = -cosine_kernel(f_t, protos_norm)
     q2 = distributed_sinkhorn(M_t, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
 
+    p1 = F.softmax(-M_s / temperature, dim=2)
+    p2 = F.softmax(-M_t / temperature, dim=2)
+
+    #p1_detach = F.softmax(-M_s_detach / temperature, dim=2)
+    #p2_detach = F.softmax(-M_t_detach / temperature, dim=2)
+    
     if distance == 'MSE':
         diff12 = q1 - p2
         diff21 = q2 - p1
         loss12 = (diff12 * diff12).mean()
         loss21 = (diff21 * diff21).mean()
     elif distance == 'KL':
-        p1 = F.softmax(-M_s / temperature, dim=2)
-        p2 = F.softmax(-M_t / temperature, dim=2)
-
-        p1_detach = F.softmax(-M_s_detach / temperature, dim=2)
-        p2_detach = F.softmax(-M_t_detach / temperature, dim=2)
-
         loss1 = - 100*temperature**2*torch.mean(torch.sum(p2_detach * torch.log(p1_detach + 1e-6), dim=2))
-        loss2 = - 100*temperature**2*torch.mean(torch.sum(q2 * torch.log(p2 + 1e-6), dim=2))
-        loss3 = - 100*temperature**2*torch.mean(torch.sum(q1 * torch.log(p1_detach + 1e-6), dim=2))
+        #loss2 = - 100*temperature**2*torch.mean(torch.sum(q2 * torch.log(p2 + 1e-6), dim=2))
+        #loss3 = - 100*temperature**2*torch.mean(torch.sum(q1 * torch.log(p1_detach + 1e-6), dim=2))
 
-    loss_mf_rand = (loss1 + loss2 + loss3)/2
-    #loss_mf_rand = (loss1 + 2*loss2)/2
+    #loss_mf_rand = (loss1 + loss2 + loss3)/2
+    #loss_mf_rand = (loss1)/2
 
     dev = loss_mf_rand.device
 
