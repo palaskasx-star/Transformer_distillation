9b8184144d528bddf3e24a006426c18290f7b79a
palaskasc <palaskasx@gmail.com>
2026-04-15 13:15:39 +0300

    Update losses.py

--- losses.py
+++ losses.py
@@ -302,6 +302,7 @@ def layer_mf_loss_prototypes_rand(F_s, F_t, K, normalize=False, distance='MSE', 
     print("Does M_s detatched requires gradients?", M_s_detach.requires_grad)
     #M_s = -cosine_kernel(f_s, protos_norm)
     q1 = distributed_sinkhorn(M_s, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
+    print(f"Q1 shape:{q1.shape}")
     M_t = L2_dist(f_t, protos_norm)
     M_t_detach = L2_dist(f_t, protos_norm.detach())
     print("Does M_t detatched requires gradients?", M_t_detach.requires_grad)
