f7ae1a225f8e81dd532b0afafe802af416f6068d
palaskasc <palaskasx@gmail.com>
2026-05-01 01:04:23 +0300

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -14,6 +14,8 @@ from torchvision.datasets.folder import ImageFolder, default_loader
 from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 from timm.data import create_transform
 
+import torch
+
 
 class IndexedDataset(torch.utils.data.Dataset):
     """
