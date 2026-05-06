f51d3f27795230a6ab77344f6388f051373910c8
palaskasc <palaskasx@gmail.com>
2026-05-06 20:58:44 +0300

    Update losses.py

--- losses.py
+++ losses.py
@@ -312,7 +312,7 @@ def layer_mf_loss_prototypes_rand(F_s, F_t, K, normalize=False, distance='MSE', 
     # ==========================================
     M_s = L2_dist(f_s, protos_unscaled)
     # q1 is detached, so it doesn't pass gradients backward anyway
-    q1 = distributed_sinkhorn(M_s, nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
+    q1 = distributed_sinkhorn(M_s.detach(), nmb_iters=3, epsilon=0.05, world_size=world_size).detach()
 
     M_t = L2_dist(f_t, protos_unscaled)
     p2 = F.softmax(-M_t / temperature, dim=2)
