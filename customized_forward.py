05cc690513d98cf4ea07a4f5eb51972ca334c1a6
palaskasc <palaskasx@gmail.com>
2026-05-07 14:21:24 +0300

    Update customized_forward.py

--- customized_forward.py
+++ customized_forward.py
@@ -1,9 +1,3 @@
-# 2022.10.14-Changed for building manifold kd
-#            Huawei Technologies Co., Ltd. <foss@huawei.com>
-#
-# Modified from Fackbook, Deit
-# {haozhiwei1, jianyuan.guo}@huawei.com
-#
 # Copyright (c) 2015-present, Facebook, Inc.
 # All rights reserved.
 #
