91484cbaaa7e738c4526fd7ce435d90a2ce20ed9
palaskasc <palaskasx@gmail.com>
2026-05-07 14:34:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # Copyright (c) 2015-present, Facebook, Inc.
 # All rights reserved.
 #
-# 2026.5.6-Changed for building ConceptKD (c) 2026
-
+# Changed for building ConceptKD (c)
 
 import argparse
 import datetime
